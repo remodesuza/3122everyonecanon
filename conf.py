2b3d0d59c44440599b24507070003fe49d4ed771
remodesuza <remodesuza@proton.me>
2026-01-19 20:34:56 +0530

    Update conf.py

--- conf.py
+++ conf.py
@@ -29,7 +29,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # -- Options for HTML output -------------------------------------------------
 
 html_theme = 'alabaster'
-html_title = "IObit Advanced SystemCare Ultimate Pro Download: Complete Installation & Activation Tutorial"
+html_title = "Fully Set Up Your Canon TS3122: Printer Installation, Wireless Configuration, and Driver Download Guide"
 
 # You can still add html_meta here if you want, but with the new Read the Docs addons
 # custom template is the reliable way to inject meta tags.
